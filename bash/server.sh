e4b84ddf31832f494824ea052de78863e35334d3
Dileepa <dileepasenarathna2@gmail.com>
2021-04-06 15:46:41 +0530

    Enable monitoring script to run between 9 AM to 5PM

--- bash/server.sh
+++ bash/server.sh
@@ -3,7 +3,9 @@ date=$(date)
 today="$( date +"%Y%m%d" )"
 filename="webserver-$today.log"
 serverdns="http://ec2-13-233-238-114.ap-south-1.compute.amazonaws.com"
-
+hour="$(date +"%k")"
+minute="$(date +"%M")"
+healthmonitor(){	
 status=$(curl -s -w '%{http_code}' -o /dev/null $serverdns)
 
 if [ $status == "200" ]; then
@@ -39,9 +41,35 @@ else
 		echo "ERROR::Server ran into issues, httpd service can't be started. Please log into the server and check"  | mail -s "SERVER ALERTS : `date`" dileepasenarathna2@gmail.com
 		status="404"
 		content="not available"
-	fi	 
-				  
+	fi
 fi
+}
+
+## Checking whether the time is between 09:00AM and 05:00PM ##
+if [ $hour -eq 11 ]; then
+	if [ $minute -ge 30 ]; then
+		echo "Health Check Script only run during 9:00AM to 5:00PM IST"
+		exit 1 
+	else
+		healthmonitor
+	fi
+elif [ $hour -gt 11 ]; then
+	echo "Health Check Script only run during 9:00AM to 5:00PM IST"
+        exit 1
+elif [ $hour -eq 3 ]; then
+	if [ $minute -lt 30 ]; then
+       		echo "Health Check Script only run during 9:00AM to 5:00PM IST"
+                exit 1
+	else
+		healthmonitor
+	fi
+elif [ $hour -lt 3 ]; then
+        echo "Health Check Script only run during 9:00AM to 5:00PM IST"
+        exit 1	
+else
+	healthmonitor
+fi
+
 #### Writing to log file###
 if [ ! -e /home/ec2-user/logs/$filename ]; then
    echo "$date:::STATUS:$status:::$statusmessage:::{Message : $content}" > /home/ec2-user/logs/$filename
